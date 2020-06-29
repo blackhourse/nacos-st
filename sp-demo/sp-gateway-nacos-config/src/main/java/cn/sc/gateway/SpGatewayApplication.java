33084526cea28639e4fb412e11766f2cbc286d33
maht <maht@citydo.com.cn>
2020-06-29 17:58:23 +0800

    添加 mysql demo

---
+++ sp-demo/sp-gateway-nacos-config/src/main/java/cn/sc/gateway/SpGatewayApplication.java
@@ -0,0 +1,16 @@
+package cn.sc.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 网关启动类
+ */
+@SpringBootApplication
+public class SpGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpGatewayApplication.class, args);
+    }
+
+}
